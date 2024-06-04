898ee1b3725f82f2acd33d79d200f0c6b7f2b090
GregOliverDev <144949275+GregOliverDev@users.noreply.github.com>
2024-06-03 21:38:08 -0300

    Alter infos mobile version

--- src/app/components/Home/HomePageComp.tsx
+++ src/app/components/Home/HomePageComp.tsx
@@ -1,17 +1,9 @@
 "use client";
 import styles from "./HomePageComp.module.css";
 import { Button } from "@mui/material";
-import Link from "next/link";
 import NextLink from "next/link";
-import { useState } from "react";
 
 export default function HomePageComp() {
-  const [menu, setMenu] = useState(false);
-
-  const handleMenuToggle = () => {
-    setMenu(!menu);
-  };
-
   return (
     <div className={styles.main}>
       <div className={styles.btMaps}>
@@ -51,36 +43,52 @@ export default function HomePageComp() {
         />
       </div>
       <div className={styles.imgMobile}>
-        <div className={styles.divMenu}>
-          <button className={styles.btMenu} onClick={handleMenuToggle}></button>
-        </div>
         <img
-          src="/assets/Home.jpg"
+          src="/assets/HomeMobile1.png"
+          alt="imgHomePage"
+          className={styles.imgHome}
+        />
+        <img
+          src="/assets/HomeMobile1.2.png"
+          alt="imgHomePage"
+          className={styles.imgHome}
+        />
+        <img
+          src="/assets/HomeMobile1.3.png"
+          alt="imgHomePage"
+          className={styles.imgHome}
+        />
+        <img
+          src="/assets/HomeMobile2.png"
+          alt="imgHomePage"
+          className={styles.imgHome}
+        />
+        <img
+          src="/assets/HomeMobile3.png"
+          alt="imgHomePage"
+          className={styles.imgHome}
+        />
+        <img
+          src="/assets/HomeMobile4.png"
+          alt="imgHomePage"
+          className={styles.imgHome}
+        />
+        <img
+          src="/assets/HomeMobile5.png"
+          alt="imgHomePage"
+          className={styles.imgHome}
+        />
+        <img
+          src="/assets/HomeMobile6.png"
+          alt="imgHomePage"
+          className={styles.imgHome}
+        />
+        <img
+          src="/assets/HomeMobile7.png"
           alt="imgHomePage"
           className={styles.imgHome}
         />
       </div>
-      {menu && (
-        <div className={styles.imgMenu}>
-          <img
-            src="/assets/Menu.jpeg"
-            alt="MenuPage"
-            className={styles.imgHome}
-          />
-          <div className={styles.divBack} onClick={handleMenuToggle}>
-
-          </div>
-          <div className={styles.divBtHome} onClick={handleMenuToggle}>
-
-          </div>
-          <Link href="/pages/Maps">
-          <div className={styles.divBtMaps} >
-            <img src="/assets/pin.png" alt="img" />
-            <p className={styles.pMaps}>Maps</p>
-          </div>
-          </Link>
-        </div>
-      )}
     </div>
   );
 }
